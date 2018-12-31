252447891bd987f746eea4fc13bf3c924861dcbb
Allan Oliveira Costa <plu_acosta@uolinc.com>
2018-12-31 12:29:51 -0200

    Tentando implementar spectron

--- test/tray.js
+++ test/tray.js
@@ -1,14 +1,24 @@
-const {Application} = require('spectron');
+const Application = require('spectron').Application
+const assert = require('assert')
+const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
+const path = require('path')
 
-describe('Inicialização da aplicação',() => {
-    
-    var app = new Application({
-        path: "C:\\workspace\\bat-host\\"
+describe('Application launch', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: "C:\\workspace\\bat-host\\dist\\win-unpacked\\BatHost.exe",
+      args: [path.join(__dirname, '..')]
     })
+    return this.app.start();
+  })
 
-    app.start().then(()=>{
-        console.log('app start');
-    });
+  afterEach(function () {
+    console.log('teste1');
+  })
 
+  it('shows an initial window', function () {
+    console.log('teste2');
+  })
 })
-
